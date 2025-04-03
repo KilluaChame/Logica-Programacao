70673ac730531630eefb8b33e3ee924dfcc324e7
KilluaChame <mateuschame9@gmail.com>
2025-04-03 16:33:04 -0300

    Terminando exercicio

--- 01-variaveis/Exercicios/src/Exercicio3.java
+++ 01-variaveis/Exercicios/src/Exercicio3.java
@@ -28,5 +28,41 @@ public class Exercicio3 {
          * - Teste diferentes valores de salário para validar a lógica condicional.
          */
 
+        /* ============================ DECLARACAO ============================ */
+
+        double Salario = 0; // Variável para armazenar o salário
+        double NovoSalario = 0; // Variável para armazenar o novo salário após o reajuste
+        int Reajuste = 0; // Variável para armazenar o percentual de reajuste
+
+        /* ========================= ENTRADA DE DADOS ========================= */
+
+        // Cria um objeto Scanner para ler a entrada do usuário pelo terminal.
+        Scanner input = new Scanner(System.in);
+
+        // Solicita e lê o salário
+        System.out.println("Digite o Salário:");
+        Salario = input.nextInt();
+
+        // Solicita e lê o percentual de reajuste
+        System.out.println("Digite o valor do sálario máximo para os 20% de aumento:");
+        Reajuste = input.nextInt();
+
+        /* ========================= SAIDA DE DADOS ========================= */
+
+        // Verifica se o salário é menor que: Reajuste
+        if (Salario < Reajuste) {
+            // Aplica um aumento de 20%
+            NovoSalario = Salario + (Salario * 0.20);
+            System.out.println("O novo salário é: " + NovoSalario);
+        } // Exibe o novo salário
+        else {
+            // Aplica um aumento de 10%
+            NovoSalario = Salario + (Salario * 0.10);
+            System.out.println("O novo salário é: " + NovoSalario); // Exibe o novo salário
+        }
+
+        // Fecha o Scanner para liberar recursos
+        input.close();
     }
+
 }
