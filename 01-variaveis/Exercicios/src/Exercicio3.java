0dc01dca891d5c95d4977c9ce99cd900d6322423
KilluaChame <mateuschame9@gmail.com>
2025-04-03 16:08:12 -0300

    Create Exercicio3.java
    Vamos lá

---
+++ 01-variaveis/Exercicios/src/Exercicio3.java
@@ -0,0 +1,32 @@
+// Classe principal do programa, sempre deve ter o mesmo nome do arquivo.
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+    // Método principal do Java, ponto de entrada da aplicação.
+    public static void main(String[] args) {
+
+        /*
+         * ============== Exercício 3: Calculadora de Reajuste Salarial ==============
+         * Desenvolva um programa que:
+         * 
+         * - Armazene um valor de salário em uma variável.
+         * - Utilize uma estrutura condicional para aplicar um reajuste:
+         * - Se o salário for menor que um certo valor (por exemplo, 2000), aplique um
+         * aumento de 20%.
+         * 
+         * - Caso contrário, aplique um aumento de 10%.
+         * - Exiba o novo salário calculado no console.
+         *
+         * Dicas:
+         * 
+         * - Divida o código em seções comentadas (por exemplo, **DECLARANDO**,
+         * CALCULANDO** e **SAÍDA NO CONSOLE**).
+         * 
+         * - Use operações aritméticas para calcular o aumento percentual.
+         * - Teste diferentes valores de salário para validar a lógica condicional.
+         */
+
+    }
+}
